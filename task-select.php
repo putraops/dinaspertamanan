e26fd8e20e5fb52313b684b644b0728fdbc5b8e7
Putra Ompusunggu <putraops@gmail.com>
2017-11-10 15:13:06 +0700

    task select by id / iduser / status & DOKUMENTASI

--- task-select.php
+++ task-select.php
@@ -14,9 +14,6 @@ try {
     if (isset($_REQUEST["iduser"])) {
         $iduser = $con->real_escape_string($_REQUEST["iduser"]);
     }
-    //if (isset($_REQUEST["idsalesmanager"])) {
-    //    $idsalesmanager = $con->real_escape_string($_REQUEST["idsalesmanager"]);
-    //}
     if (isset($_REQUEST["status"])) {
         $status = $con->real_escape_string($_REQUEST["status"]);
     }
@@ -29,27 +26,31 @@ try {
 
     $sql  = "Select t.id as id, 
                     t.taskname as namatask,
-                    t.createduser as createduser,
+                    t.detailtask as detailtask,
                     t.tanggaldibuat as tanggaldibuat, 
                     t.statustask as statustask,  
                     u.nama as creatednameuser, 
-                    u.id as createduserid 
-            FROM task t";
-    $sql .= " INNER JOIN user u ON t.createduser = u.id";
+                    u.id as createduserid, 
+                    up.nama as namasopir, 
+                    up.id as sopirid,
+                    k.nama as namakendaraan,
+                    k.no_polisi as nopolis
+            FROM task t 
+            INNER JOIN taskkendaraan tk ON tk.taskid = t.id
+            INNER JOIN kendaraan k ON k.id = tk.kendaraanid
+            INNER JOIN user up ON up.id = tk.userid
+            INNER JOIN user u ON t.createduser = u.id";
     $sql .= " WHERE t.id > 0 AND t.statustask != 'Closed'";
     //echo $sql;exit;
     if ($status != "") {
         $sql .= " AND t.statustask = '$status'";
     }
     if ($iduser != "") {
-        $sql .= " AND td.userid = '$iduser'";
+        $sql .= " AND tk.userid = '$iduser'";
     }
-
     if ($id != "") {
         $sql .= " AND t.id = '$id'";
     }
-
-    //echo $sql;exit;
 
     $result = $con->query($sql);
 
@@ -62,28 +63,20 @@ try {
                 'namatask' => $row['namatask'],
                 'tanggaldibuat' => $row['tanggaldibuat'],
                 'statustask' => $row['statustask'],
+                'detailtask' => $row['detailtask'],
                 'dibuatolehid' => $row['createduserid'],
                 'dibuatoleh' => $row['creatednameuser'],
-                //'totalpekerja' => $resulttotal->num_rows
+                'namasopir' => $row['namasopir'],
+                'sopirid' => $row['sopirid']
             ];
             
-            
-            //$sqltotalpekerja = "SELECT * FROM task_detail WHERE taskid = '$taskid' ";
-
-            //if ($iduser != "") {
-            //   $sqltotalpekerja .= " AND userid = '$iduser'";
-            //}
-
-            //$resulttotal = $con->query($sqltotalpekerja);
-
-
             array_push($arrResult, $data);
 
             if ($id == "" || $id == 0) {
                 
             } else if ($id > 0) {
                 ## DETAIl PEKERJAAN
-                $sql = "SELECT id, lokasi, latitude, longitude, statusdikerjakan 
+                $sql = "SELECT id, lokasi, latitude, longitude, status 
                             FROM task_detail 
                             WHERE taskid = '$id'";
                 $sql .= " ORDER BY id ASC;";
@@ -95,35 +88,12 @@ try {
                         $data = [
                             'id' => $row['id'],
                             'idtask' => $id,
-                            'lokasi' => $row['statusdikerjakan'],
+                            'lokasi' => $row['lokasi'],
                             'latitude' => $row['latitude'],
                             'longitude' => $row['longitude'],
-                            'status' => $row['statusdikerjakan']
+                            'status' => $row['status']
                         ];
                         array_push($arrPengerjaan, $data);
-                        
-                        ##
-                        ## ambil list pekerjaan dalam detail task
-                        ##
-                        
-//                        $listpengerjaan_query = "SELECT id, taskdetailid, ext, tipe, created_at FROM pengerjaan WHERE taskdetailid =".$row['id'];
-//                       // $listpengerjaan_query = "UPDATE table SET commodity_quantity=".$qty."WHERE user=".$rows['user'];
-//
-//                        $result_pengerjaan = $subinner_connection->query($listpengerjaan_query);
-//
-//                        if ($result_pengerjaan->num_rows > 0) {
-//                            while ($row_pengerjaan = $result_pengerjaan->fetch_assoc()) {
-//                                $data = [
-//                                    'id' => $row_pengerjaan['id'],
-//                                    'taskdetailid' => $row_pengerjaan['taskdetailid'],
-//                                    'fileupload' => $_SERVER['SERVER_NAME'] . "\\" . "uploads" . "\\" . $row_pengerjaan['id'] . $row_pengerjaan['ext'],
-//                                    'tipe' => $row_pengerjaan['tipe'],
-//                                    'waktuabsen' => $row_pengerjaan['created_at']
-//                                ];
-//                                array_push($arrPengerjaan, $data);
-//                            }
-//                        }
-                        ## END of [ambil list pekerjaan dalam detail task]
 
                     }
                 }
